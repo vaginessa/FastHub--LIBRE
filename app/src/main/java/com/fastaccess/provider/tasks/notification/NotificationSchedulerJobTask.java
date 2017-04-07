41925e29504a17ad209cf19bd6b676ca13cb6cd9
thermatk <thermatk@thermatk.com>
2017-04-07 17:28:58 +0200

    bug: you had it in seconds, sorry :)

--- app/src/main/java/com/fastaccess/provider/tasks/notification/NotificationSchedulerJobTask.java
+++ app/src/main/java/com/fastaccess/provider/tasks/notification/NotificationSchedulerJobTask.java
@@ -76,7 +76,7 @@ public class NotificationSchedulerJobTask extends Job {
             int jobId =  new JobRequest.Builder(NotificationSchedulerJobTask.TAG)
                     .setPersisted(true)
                     .setRequiredNetworkType(JobRequest.NetworkType.CONNECTED)
-                    .setPeriodic(TimeUnit.MINUTES.toMillis(duration))
+                    .setPeriodic(TimeUnit.SECONDS.toMillis(duration))
                     .build()
                     .schedule();
         }
